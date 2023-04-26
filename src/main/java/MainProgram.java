bd02e6a149c8b1ac5c4be9ed400d0b97b42251c3
Moppieee <troxisai@kgg-do.eu>
2023-04-26 19:13:56 +0200

    oooooof

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -1,11 +1,27 @@
 import KAGO_framework.Config;
 import KAGO_framework.control.MainController;
+import javafx.scene.transform.Shear;
 
 public class MainProgram {
 
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
         MainController.startFramework();
     }
 
+    public void draw(DrawTool drawTool) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if ((i + j%2) % 2 == 0) {
+                    drawTool.setCurrentColor(Color.WHITE);
+                }else {
+                    drawTool.setCurrentColor(Color.BLACK);
+                }
+                drawTool.drawFilledRectangle(isquareSize,jsquareSize,squareSize,squareSize);
+
+            }
+        }
+    }
 }
+
+
