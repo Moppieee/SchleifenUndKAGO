f92d107be400b1eac04550619b27201daa71d549
Moppieee <troxisai@kgg-do.eu>
2023-05-02 15:44:31 +0200

    funzi

--- src/main/java/my_project/model/Grid.java
+++ src/main/java/my_project/model/Grid.java
@@ -10,16 +10,16 @@ import java.awt.*;
 import java.sql.SQLOutput;
 
 public class Grid extends GraphicalObject {
-    int size = 12;
+    int size = 12;                              //Die Anzahl der Squares pro Seite
     public void draw(DrawTool drawTool) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if ((i + j%2) % 2 == 0) {
+                if ((i + j%2) % 2 == 0) {  //Modulo rechnung--> bestimmt die Farbe des Rasters
                     drawTool.setCurrentColor(Color.WHITE);
                 }else {
                     drawTool.setCurrentColor(Color.BLACK);
                 }
-                drawTool.drawFilledRectangle(50 * j, i * 50,50,50);
+                drawTool.drawFilledRectangle(50 * j, i * 50,50,50);  //Die einzelnen Squares
 
             }
         }
